574206ae0777819cbedf205449d82c51313df2f9
romnoelp <petracorta.rmnl@gmail.com>
2024-12-06 10:12:57 +0800

    Add error page, login form, and utility functions; update Tailwind config and package dependencies

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from "@/components/client-components/login-form";
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
