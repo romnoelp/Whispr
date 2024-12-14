724906f5974e50fbf48d1703d8eb5dd5df92b002
romnoelp <petracorta.rmnl@gmail.com>
2024-12-15 00:04:17 +0800

    Refactor layout and UI components: update app title, remove cat fact API, and add new skeleton, separator, tooltip, and sheet components.

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,13 @@
 "use client";
 
-import { LogoutButton } from "@/components/server-components/logout-button";
 import { Button } from "@/components/ui/button";
-import React, { useState } from "react";
 
 const Home = () => {
-  const [catData, setCatData] = useState("");
 
-  const fetchCatData = async () => {
-    try {
-      const response = await fetch("/api/fetch-cat-fact"); 
-      const data = await response.json();
-      console.log("Cat Fact:", data.message); 
-      setCatData(data.message); 
-    } catch (error) {
-      console.error("Error fetching cat data:", error);
-    }
-  };
+
 
   return (
     <div>
-      <LogoutButton />
-      <Button onClick={fetchCatData}>Fetch cat data</Button>
-      {catData && <p>{catData}</p>} 
     </div>
   );
 };
